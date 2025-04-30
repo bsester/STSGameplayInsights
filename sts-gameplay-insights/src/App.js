3974bb7b9d434618aec3fdf10a2bfc574ff376c2
bsester <124089310+bsester@users.noreply.github.com>
2025-04-29 19:58:36 -0500

    bootstrap installation

--- sts-gameplay-insights/src/App.js
+++ sts-gameplay-insights/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 const sampleRun = require('./sampleRun.json');
@@ -14,7 +13,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
             Deck Composition:
         </p>
